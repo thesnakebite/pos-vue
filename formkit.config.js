3075a56bdf09a90f4c239cfe21f09ae59a59c626
Álvaro Navarro <alvaro.navarro@manifiesto.biz>
2023-12-30 18:26:49 +0100

    How to identify sections in Formkit and apply custom CSS

--- formkit.config.js
+++ formkit.config.js
@@ -1,5 +1,15 @@
+import { generateClasses } from '@formkit/themes'
+
 const config = {
     config: {
+        classes: generateClasses({
+            text: {
+                message: 'text-red-500',
+            },
+            number: {
+                message: 'text-red-500',
+            },
+        })
 
     }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./formkit.config.js"
   ],
   theme: {
     extend: {},

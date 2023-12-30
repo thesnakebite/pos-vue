063f942ff75da9e74fb0a65f72583423a2241131
Álvaro Navarro <alvaro.navarro@manifiesto.biz>
2023-12-30 14:16:32 +0100

    Adding fields to new products

--- src/views/admin/NewProductView.vue
+++ src/views/admin/NewProductView.vue
@@ -10,12 +10,66 @@
         </Link>
         <h1 class="text-4xl font-black my-10">Nuevo producto</h1>
 
-        <FormKit
-            type="form"
-        
-        >
+        <div class="flex justify-center bg-white shadow">
+            <div class="mt-10 p-10 w-full 2xl:w-2/4">
+                <FormKit
+                    type="form"
+                    submit-label="Agregar producto"
+                
+                >
+                    <FormKit
+                        type="text"
+                        label="Nombre"
+                        name="name"
+                        placeholder="Nombre del producto"
+                        validation="required"
+                        :validation-messages="{required: 'El nombre del producto es Obligatorio'}"
+                    />
+                    <FormKit
+                        type="file"
+                        label="Imagen del producto"
+                        name="image"
+                        placeholder="Nombre del producto"
+                        validation="required"
+                        :validation-messages="{required: 'La imagen del producto es Obligatorio'}"
+                        accept="jpg, jpeg, png"
+                        multiple="false"
+                    />
+                    <FormKit
+                        type="select"
+                        label="Categoría"
+                        name="category"
+                        validation="required"
+                        :validation-messages="{required: 'La categoría es Obligatoria'}"
+                        :options="[
+                            {value: '1', label: 'Categoría 1'},
+                            {value: '2', label: 'Categoría 2'},
+                            {value: '3', label: 'Categoría 3'},
+                            {value: '4', label: 'Categoría 4'},
+                        ]"
+                    />
+                    <FormKit
+                        type="number"
+                        label="Precio"
+                        name="price"
+                        placeholder="Precio del producto"
+                        validation="required"
+                        :validation-messages="{required: 'El precio del producto es Obligatorio'}"
+                        min="1"
+                    />
+                    <FormKit
+                        type="number"
+                        label="Disponibles"
+                        name="availability"
+                        placeholder="Cantidad disponible"
+                        validation="required"
+                        :validation-messages="{required: 'La cantidad es Obligatoria'}"
+                        min="1"
+                    />
 
 
-        </FormKit>
+                </FormKit>
+            </div>
+        </div>
     </div>
 </template>

7b880eacfa84b2708d9eedb75cba935333fb901a
Álvaro Navarro <alvaro.navarro@manifiesto.biz>
2023-12-30 20:53:24 +0100

    adding firebase

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+// Firebase
+import { VueFire, VueFireAuth } from 'vuefire'
+import { firebaseApp } from './config/firebase'
+
 import App from './App.vue'
 import router from './router'
 import { plugin, defaultConfig } from '@formkit/vue'
@@ -10,6 +14,10 @@ import config from '../formkit.config'
 
 const app = createApp(App)
 
+app.use(VueFire, {
+    firebaseApp,
+    modules: [VueFireAuth()],
+  })
 app.use(createPinia())
 app.use(router)
 app.use(plugin, defaultConfig(config))
